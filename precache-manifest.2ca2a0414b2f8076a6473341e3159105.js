dedc5c546f5cf3852d063ad9d9f23acd00330aaa
gieoon <jun.a.kagaya@gmail.com>
2020-07-27 00:00:52 +1200

    Updates

---
+++ precache-manifest.2ca2a0414b2f8076a6473341e3159105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e056118aabfc321ace4b1fd89a1886b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d0eb55dbced75b07ae6",
+    "url": "./static/css/main.bc8579ad.chunk.css"
+  },
+  {
+    "revision": "61fd007b262344791b52",
+    "url": "./static/js/2.3303d94b.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "./static/js/2.3303d94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0eb55dbced75b07ae6",
+    "url": "./static/js/main.36924110.chunk.js"
+  },
+  {
+    "revision": "1ca25381a9daf98dd72a",
+    "url": "./static/js/runtime-main.69edb7e3.js"
+  }
+]);
